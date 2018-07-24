a9cd10ac7d2946934d12a705422aa85899d3b028
tihangrai <tihangrai@gmail.com>
2018-07-24 01:33:48 -0400

    deploy prep

--- server.js
+++ server.js
@@ -24,6 +24,16 @@ app.get('/', (req, res) => {
     res.send({title: "Welcome to my app"});
 });
 
+//Server static assets if in porduction
+if(process.env.NODE_ENV == 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
 
 const PORT = process.env.PORT || 5000;
 
