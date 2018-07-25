a76f37c50713f2fc04b0b66767389e1eddbd6ecb
tihangrai <tihangrai@gmail.com>
2018-07-25 18:47:15 -0400

    procfile edit

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: npm run server.js
+web: node server.js
--- server.js
+++ server.js
@@ -34,9 +34,6 @@ if(process.env.NODE_ENV == 'production'){
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
-    module.exports = require('./config/prod');
-}else{
-    module.exports = require('./config/keys');
 }
 
 
